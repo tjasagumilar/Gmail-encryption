c5d78e5f83e9e63004b75754cde1ccf87bc5fb35
tjasag <tjasa.gumilar@student.um.si>
2023-04-11 14:05:27 +0200

    Decryption dela

--- projekt.js
+++ projekt.js
@@ -1,6 +1,5 @@
 InboxSDK.load('2', 'sdk_GmailAPI_f30f8371ce').then(function (sdk) {
 
-
     sdk.Compose.registerComposeViewHandler(function (composeView) {
 
         composeView.addButton({
@@ -17,7 +16,6 @@ InboxSDK.load('2', 'sdk_GmailAPI_f30f8371ce').then(function (sdk) {
                 var publicKeyPrejemnikaArmored = null;
 
                 chrome.storage.sync.get('vsiuporabniki', async function (result) {
-                    // console.log(result.vsiuporabniki);
                     var uporabniki = result.vsiuporabniki;
 
                     for (var i = 0; i < uporabniki.length; i++) {
@@ -28,17 +26,18 @@ InboxSDK.load('2', 'sdk_GmailAPI_f30f8371ce').then(function (sdk) {
 
                     const publicKeyPrejemnika = await openpgp.readKey({ armoredKey: publicKeyPrejemnikaArmored }); //dela
 
-
                     const encrypted = await openpgp.encrypt({
-                        message: await openpgp.createMessage({ text: sporocilo }), 
+                        message: await openpgp.createMessage({ text: sporocilo }),
                         encryptionKeys: publicKeyPrejemnika,
                     });
+
                     event.composeView.setBodyText(encrypted);
 
                 });
             },
         });
     });
+
 
     sdk.Conversations.registerMessageViewHandler(function (MessageView) {
 
@@ -48,6 +47,46 @@ InboxSDK.load('2', 'sdk_GmailAPI_f30f8371ce').then(function (sdk) {
             title: 'Decrypt',
             onClick: function () {
 
+                const sporociloKriptirano = MessageView.getBodyElement(); //dela
+                console.log(sporociloKriptirano.textContent);
+
+                const prejemniki = MessageView.getRecipientEmailAddresses(); //dela
+                const prejemnik = prejemniki[0];
+                console.log(prejemnik);
+
+                var privateKeyPrejemnikaArmored = null;
+                var passphrase = null;
+
+                chrome.storage.sync.get('vsiuporabniki', async function (result) {
+                    var uporabniki = result.vsiuporabniki;
+
+                    for (var i = 0; i < uporabniki.length; i++) {
+                        if (uporabniki[i].email == prejemnik) {
+                            privateKeyPrejemnikaArmored = uporabniki[i].privateKey;
+                            passphrase = uporabniki[i].passphrase;
+                        }
+                    }
+
+                    const privateKey = await openpgp.decryptKey({ //dela
+                        privateKey: await openpgp.readPrivateKey({ armoredKey: privateKeyPrejemnikaArmored }),
+                        passphrase
+                    });
+
+                 
+                    const message = await openpgp.readMessage({
+                        armoredMessage: sporociloKriptirano.textContent 
+                    });
+
+                    const { data: decrypted, signatures } = await openpgp.decrypt({
+                        message,
+                        decryptionKeys: privateKey
+                    });
+
+
+                    console.log(decrypted);
+                    sporociloKriptirano.textContent = decrypted; 
+                    
+                });
             }
         });
     });
