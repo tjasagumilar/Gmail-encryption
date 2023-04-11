deb89a7c54ebb304047702796665d09dfa2909f2
tjasag <tjasa.gumilar@student.um.si>
2023-04-11 18:20:56 +0200

    Podpisovanje - dela

--- projekt.js
+++ projekt.js
@@ -38,6 +38,51 @@ InboxSDK.load('2', 'sdk_GmailAPI_f30f8371ce').then(function (sdk) {
         });
     });
 
+    sdk.Compose.registerComposeViewHandler(function (composeView) {
+
+        composeView.addButton({
+            title: "Sign message",
+            iconUrl: 'https://pbs.twimg.com/profile_images/599482221402161152/ZIUk-8TB.png',
+            onClick: async function (event) {
+
+                const sporocilo = event.composeView.getTextContent(); //dela
+               // console.log(sporocilo);
+
+                const posiljatelj = event.composeView.getFromContact().emailAddress; //dela
+               // console.log(posiljatelj);
+
+                var privateKeyPosiljateljaArmored = null;
+                var passphrase = null;
+
+                chrome.storage.sync.get('vsiuporabniki', async function (result) {
+                    var uporabniki = result.vsiuporabniki;
+
+                    for (var i = 0; i < uporabniki.length; i++) {
+                        if (uporabniki[i].email == posiljatelj) {
+                            privateKeyPosiljateljaArmored = uporabniki[i].privateKey;
+                            passphrase = uporabniki[i].passphrase;
+                        }
+                    }
+
+                    const privateKey = await openpgp.decryptKey({ 
+                        privateKey: await openpgp.readPrivateKey({ armoredKey: privateKeyPosiljateljaArmored }),
+                        passphrase
+                    });
+
+                    const unsignedMessage = await openpgp.createCleartextMessage({ text: sporocilo });
+                    const cleartextMessage = await openpgp.sign({
+                        message: unsignedMessage, 
+                        signingKeys: privateKey
+                    });
+
+                    event.composeView.setBodyText(cleartextMessage);
+
+                });
+
+
+            },
+        });
+    });
 
     sdk.Conversations.registerMessageViewHandler(function (MessageView) {
 
@@ -72,9 +117,9 @@ InboxSDK.load('2', 'sdk_GmailAPI_f30f8371ce').then(function (sdk) {
                         passphrase
                     });
 
-                 
+
                     const message = await openpgp.readMessage({
-                        armoredMessage: sporociloKriptirano.textContent 
+                        armoredMessage: sporociloKriptirano.textContent
                     });
 
                     const { data: decrypted, signatures } = await openpgp.decrypt({
@@ -83,10 +128,23 @@ InboxSDK.load('2', 'sdk_GmailAPI_f30f8371ce').then(function (sdk) {
                     });
 
 
-                    console.log(decrypted);
-                    sporociloKriptirano.textContent = decrypted; 
-                    
+                    //console.log(decrypted);
+                    sporociloKriptirano.textContent = decrypted;
+
                 });
+            }
+        });
+    });
+
+    sdk.Conversations.registerMessageViewHandler(function (MessageView) {
+
+        MessageView.addToolbarButton({
+
+            section: 'MORE',
+            title: 'Verify',
+            onClick: function () {
+
+
             }
         });
     });
